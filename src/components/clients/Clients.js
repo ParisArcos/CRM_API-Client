33c7d2ffb8bcd5085ddc496c8bb09f1acdd4e64d
Enrique Hidalgo <koko.freelance@gmail.com>
2022-01-11 19:29:48 +0000

    build-> cors, APIcall

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import clientAxios from "../../config/axios"
+
 
 const Clients = () => {
+
+  const [clients, setClients] = useState([])
+
+
+
+  const APIcall = async () => {
+    const clientsReq = await clientAxios.get("/clients")
+    // console.log(clientsReq.data)
+
+    setClients(clientsReq.data)
+  }
+
+  useEffect(() => {
+    APIcall()
+  }, []);
+
   return (
     <div>
       <h2>This is Clients component</h2>
+      <ul>
+        {clients.map(client => {
+          return console.log(client)
+        })}
+      </ul>
     </div>
   );
 };
