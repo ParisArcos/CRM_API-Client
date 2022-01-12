7283a9387403cf7eb4cc83cf4582833c319efb72
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2022-01-12 15:30:56 +0000

    fix-> newClient form Bug

--- src/components/clients/NewClient.js
+++ src/components/clients/NewClient.js
@@ -1,59 +1,104 @@
-import React, { useEffect, useState } from 'react'
-import clientAxios from '../../config/axios'
+import React, { useState } from "react";
 
 const NewClient = () => {
+  /**
+   *  newClient = state  setNewClient = setState
+   */
+  const [newClient, setNewClient] = useState({
+    name: "",
+    lastName: "",
+    company: "",
+    email: "",
+    phoneNumber: "",
+  });
 
-  const [newClient, setNewClient] = useState({})
+  // handleChange
+  const handleChange = (e) => {
+    setNewClient({
+      //* actual state
+      ...newClient,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const { name, lastName, company, email, phoneNumber } = newClient
+  const validateForm = () => {
+    const { name, lastName, company, email, phoneNumber } = newClient;
+    //todo improve function
+    let validate =
+      !name.length ||
+      !lastName.length ||
+      !company.length ||
+      !email.length ||
+      !phoneNumber.length;
 
-  const APIcall = async () => {
-    const clientsReq = await clientAxios.post("/api/clients")
-    // console.log(clientsReq.data)
+    return validate;
+  };
 
-    setNewClient(clientsReq.data)
-  }
-
-  useEffect(() => {
-    APIcall()
-  }, []);
   return (
     <div>
       <h2>Add New Client</h2>
-      <form action={APIcall()} method="POST">
-
+      <form>
         <div className="campo">
           <label>Name:</label>
-          <input type="text" placeholder="Name Cliente" name={name} />
+          <input
+            type="text"
+            placeholder="Name Cliente"
+            name="name"
+            onChange={handleChange}
+          />
         </div>
 
         <div className="campo">
           <label>Last Name:</label>
-          <input type="text" placeholder="Apellido Cliente" name={lastName} />
+          <input
+            type="text"
+            placeholder="Apellido Cliente"
+            name="lastName"
+            onChange={handleChange}
+          />
         </div>
 
         <div className="campo">
           <label>Company:</label>
-          <input type="text" placeholder="Empresa Cliente" name={company} />
+          <input
+            type="text"
+            placeholder="Empresa Cliente"
+            name="company"
+            onChange={handleChange}
+          />
         </div>
 
         <div className="campo">
           <label>Email:</label>
-          <input type="email" placeholder="Email Cliente" name={email} />
+          <input
+            type="email"
+            placeholder="Email Cliente"
+            name="email"
+            onChange={handleChange}
+          />
         </div>
 
         <div className="campo">
           <label>Phone Number:</label>
-          <input type="text" placeholder="Teléfono Cliente" name={phoneNumber} />
+          <input
+            type="text"
+            placeholder="Teléfono Cliente"
+            name="phoneNumber"
+            onChange={handleChange}
+          />
         </div>
 
         <div className="enviar">
-          <input type="submit" className="btn btn-azul" value="Add Client" />
+          <input
+            type="submit"
+            className="btn btn-azul"
+            value="Add Client"
+            disabled={validateForm()}
+          />
         </div>
-
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewClient
+export default NewClient;
