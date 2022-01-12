d02687b90393e3d3b3fc1ea634e1834a68fcd132
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2022-01-12 22:19:36 +0000

    feat->clients CRUD completed

--- src/components/clients/Client.js
+++ src/components/clients/Client.js
@@ -1,7 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import clientAxios from "../../config/axios";
+
 const Client = ({ client }) => {
   const { _id, name, lastName, email, company, phoneNumber } = client;
+
+  /**
+   * This function sends request to API for delete clients
+   */
+  const deleteClient = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clientAxios.delete(`/clients/${id}`).then((res) => {
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        });
+      }
+    });
+  };
+
   return (
     <li className="cliente">
       <div className="info-cliente">
@@ -17,7 +42,11 @@ const Client = ({ client }) => {
           <i className="fas fa-pen-alt"></i>
           Editar Cliente
         </Link>
-        <button type="button" className="btn btn-rojo btn-eliminar">
+        <button
+          type="button"
+          className="btn btn-rojo btn-eliminar"
+          onClick={() => deleteClient(_id)}
+        >
           <i className="fas fa-times"></i>
           Eliminar Cliente
         </button>
--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,37 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom"
-import clientAxios from "../../config/axios"
+import { Link } from "react-router-dom";
+import clientAxios from "../../config/axios";
 import Client from "./Client";
 
-
-
 const Clients = () => {
-
-  const [clients, setClients] = useState([])
-
-
+  const [clients, setClients] = useState([]);
 
   const APIcall = async () => {
-    const clientsReq = await clientAxios.get("/clients")
+    const clientsReq = await clientAxios.get("/clients");
     // console.log(clientsReq.data)
 
-    setClients(clientsReq.data)
-  }
+    setClients(clientsReq.data);
+  };
 
   useEffect(() => {
-    APIcall()
-  }, []);
+    APIcall();
+  }, [clients]);
 
   return (
     <div>
-
       <h2>This is Clients component</h2>
 
       <Link to="/clients/new">Add New Client</Link>
 
       <ul className="listado-clientes">
-        {clients.map(client => {
-          return <Client key={client._id} client={client} />
+        {clients.map((client) => {
+          return <Client key={client._id} client={client} />;
         })}
       </ul>
     </div>
